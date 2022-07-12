efba0ad10bfb483d3db3788ef47df0b4e8a9fed2
Makhate Makhate <makhatemakhate@gmail.com>
2022-07-12 12:25:48 +0200

    adding Kafka installation

--- distro.sh
+++ distro.sh
@@ -9,3 +9,10 @@ yum install emacs;
 
 # JDK 11
 yum -y install java-11-openjdk java-11-openjdk-devel
+
+# installing Kafka
+cd ~
+wget https://archive.apache.org/dist/kafka/3.1.0/kafka_2.13-3.1.0.tgz
+tar xzvf kafka_2.13-3.1.0.tgz
+echo PATH="$PATH:~/kafka_2.13-3.1.0/bin" > ~/.bashrc
+source ~/.bashrc
